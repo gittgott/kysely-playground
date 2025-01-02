7d52282298e3569ff907ee54b7a13704030e3bee
Tyler Gottfredsen <tyler.gottfredsen@belmark.com>
2025-01-02 15:49:04 -0600

    Updating base url to /kysely-codegen/

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
   plugins: [visualizer()],
+  base: "/kysely-playground/",
   build: {
     sourcemap: false,
     rollupOptions: {
